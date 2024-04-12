77a1ec8d491de12ccc61c2cb99afd807b91d7ab4
Pedro Carlos <58538765+PedroCarlos30@users.noreply.github.com>
2024-04-12 09:28:41 -0300

    matriz.c

--- matriz.c
+++ matriz.c
@@ -3,13 +3,7 @@
 
 int main(void) {
 	setlocale(LC_ALL, "Portuguese");
-	int mat[5][5], l, c, i = 0;
-		for(l = 0; l < 5; l++){
-		for(c = 0; c < 5; c++){
-			printf("Digite o %dº valor: ", i++);
-			scanf("%d", &mat[l][c]);
-		}
-	}
+	int mat[5][5] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25}, l, c, i = 0;
 	printf("\nMatriz\n\n");
 	for(l = 0; l < 5; l++){
 		for(c = 0; c < 5; c++){
